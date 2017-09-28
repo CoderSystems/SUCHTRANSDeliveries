4a324f60f763e20e741e9a4f17f2b1a0245aef42
Karishma <31471519+Karishma010@users.noreply.github.com>
2017-09-28 12:32:45 +0200

    Update AdminController.cs
    Editing customer loyalty points

--- Inc2SuchTrans/Controllers/AdminController.cs
+++ Inc2SuchTrans/Controllers/AdminController.cs
@@ -748,5 +748,87 @@ namespace Inc2SuchTrans.Controllers
                 return RedirectToAction("JobQueue", "Admin");
             }
         }
+                /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AuthLog(Roles = "Admin")]
+        public ActionResult CustomerEdit(int? id)
+        {
+            try
+            {
+                Customer cust = cLogic.searchCustomer(id);
+                return View(cust);
+            }
+            catch (Exception e)
+            {
+                Danger("Oops! SOmething went wrong.. Please contact support");
+                return View();
+                throw e;
+            }
+        }
+
+         [AuthLog(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult CustomerEdit(int id, string CustomerName, string CustomerSurname, string IDNumber, string City, string CustomerAddress, string PostalCode, string Email, string ContactNumber, string LoyaltyPoints)
+        {
+            if (String.IsNullOrEmpty(CustomerName))
+            {
+                Danger("Please Enter Your Name!");
+                Customer cust = cLogic.searchCustomer(id);
+                return View(cust);
+            }
+
+            try
+            {
+                Customer cust = cLogic.searchCustomer(id);
+                if (cust != null)
+                {
+                    cust.UserID = User.Identity.GetUserId();
+                    cust.CustomerName = CustomerName;
+                    cust.CustomerSurname = CustomerSurname;
+                    cust.IDNumber = IDNumber;
+                    cust.City = City;
+                    cust.CustomerAddress = CustomerAddress;
+                    cust.PostalCode = PostalCode;
+                    cust.Email = Email;
+                    cust.ContactNumber = ContactNumber;
+                    cust.LastModified = System.DateTime.Now;
+                    cust.LoyaltyPoints = Convert.ToInt16(LoyaltyPoints);
+                    cLogic.updateDetails(cust);
+                }
+                Success("Successfully Updated Details!");
+                return RedirectToAction("CustomerDetails", new { id = cust.CustomerID });
+            }
+            catch (Exception e)
+            {
+                Danger("Oops! Something went wrong.. <br> Please contact support..");
+                return View();
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AuthLog(Roles = "Admin")]
+        public ActionResult CustomerDetails(int? id)
+        {
+            try
+            {
+                id = cLogic.getCurrentUserId(User.Identity.GetUserId());
+                Customer cust = cLogic.searchCustomer(id);
+                return View(cust);
+            }
+            catch (Exception e)
+            {
+                Danger("Oops! Something went wrong.. <br> Cannot retrieve your details. Please contact support..");
+                return View();
+                throw e;
+            }
+        }
     }
 }
